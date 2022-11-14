9ecd4e4b34c0ea192692b546573a15dd5768a548
Gleb Tishchenko <gleb.tishchenko@eficode.com>
2022-11-14 12:15:55 +0200

    creating joke.js and main.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const request = require("request-promise")
+
+const options = {
+    method: "GET",
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+        Accept: "application/json",
+        "User/Agent": "Writing JS action GitHub Skills course."
+    },
+    json: true
+};
+
+async function getAJoke() {
+    const res = await request(options);
+    return res.joke;
+}
+
+module.exports = getAJoke;
